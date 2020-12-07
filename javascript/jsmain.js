84c4fb9ae9f72582a1d2481f534953b708ff45f3
Your Name <you@example.com>
2020-12-07 18:08:17 +0000

    Adjusted header, footer and navigational bar, the latter 2 making us eof divs for formatting. Added extra navigation links to the footer, as well as added images for use within the navigation bar. Also added image rollover to the navigation bar to give user feedback when they mouse over the images within the nav bar, indicating to them that these images are links to other pages of the website

--- javascript/jsmain.js
+++ javascript/jsmain.js
@@ -8,3 +8,27 @@ function check(){
 		div.style.display = "none";	
 	}
 }
+function MouseRollover1(HomeImage) {
+  HomeImage.src = "images/homebuttonmouseover.png";
+}
+function MouseOut1(HomeImage){
+  HomeImage.src = "images/homebutton.png";
+}
+function MouseRollover2(ProjectsImage) {
+  ProjectsImage.src = "images/projectsbuttonmouseover.png";
+}
+function MouseOut2(ProjectsImage){
+  ProjectsImage.src = "images/projectsbutton.png";
+}
+function MouseRollover3(InterestsImage) {
+  InterestsImage.src = "images/interestsbuttonmouseover.png";
+}
+function MouseOut3(InterestsImage){
+  InterestsImage.src = "images/interestsbutton.png";
+}
+function MouseRollover4(ContactImage) {
+  ContactImage.src = "images/contactbuttonmouseover.png";
+}
+function MouseOut4(ContactImage){
+  ContactImage.src = "images/contactbutton.png";
+}
